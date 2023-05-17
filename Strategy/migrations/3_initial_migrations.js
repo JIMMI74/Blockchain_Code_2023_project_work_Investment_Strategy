70a4fb30c05d20bf4492ccbe329c1e95152ae80e
JIMMI74 <giuseppe.fontana2006@gmail.com>
2023-05-17 23:33:21 +0200

    test 2

--- Strategy/migrations/3_initial_migrations.js
+++ Strategy/migrations/3_initial_migrations.js
@@ -17,30 +17,36 @@ module.exports = async function (deployer, network, accounts) {
   const StrategyTwoContract = await StrategyTwo.deployed();
   console.log("Deployed StrategyTWO at address: ", StrategyTwoContract.address);
 
-  // Transfer Ak Token => StrategyTwo contract
-  const transferAkAmount = web3.utils.toBN('2200');
-  await akToken.transfer(StrategyTwoContract.address, transferAkAmount);
-  console.log(`Transferred ${transferAkAmount} Ak tokens to StrategyTWO contract`, StrategyTwoContract.address);
+  console.log({ accounts })
+  await akToken.mint(StrategyTwoContract.address, web3.utils.toBN('2000000000000000000'))
 
-  const usdtCashAmount = web3.utils.toBN('33000000000000000000');
+  await akToken.mint(accounts[0], web3.utils.toBN('123'))
+  await akToken.mint(accounts[1], web3.utils.toBN('234'))
+  await akToken.mint(accounts[2], web3.utils.toBN('345'))
+  await akToken.mint(accounts[3], web3.utils.toBN('456'))
+  await akToken.mint(accounts[4], web3.utils.toBN('567'))
+  // transfer ownership 
+
+  /*
+    // Transfer Ak Token => StrategyTwo contract
+    const transferAkAmount = web3.utils.toBN('1000000000000000000');
+    await akToken.transfer(StrategyTwoContract.address, transferAkAmount);
+    console.log(`Transferred ${transferAkAmount} Ak tokens to StrategyTWO contract`, StrategyTwoContract.address);
+  */
+  const usdtCashAmount = web3.utils.toBN('3000000000000000000');
   await usdtCash.transfer(StrategyTwoContract.address, usdtCashAmount);
   console.log(`Transferred ${usdtCashAmount} USDT tokens to StrategyTWO contract`, StrategyTwoContract.address);
 
 
   // Transfer some tokens to accounts for testing purposes
-  const trasferUsdtCashAmount = web3.utils.toBN('440000000000000000000');
+  const trasferUsdtCashAmount = web3.utils.toBN('44000000000000000000');
   await usdtCash.transfer(accounts[1], trasferUsdtCashAmount);
   console.log(usdtCash.address, accounts[1], trasferUsdtCashAmount)
-
-  // Transfer some tokens to accounts for testing purposes
-  //const trasferAkkTokenAmount = web3.utils.toBN('123');
-  //await akToken.transfer(accounts[1], trasferAkkTokenAmount);
-  //console.log(akToken.address, accounts[1], trasferAkkTokenAmount)
 
   await usdtCash.transfer(accounts[2], trasferUsdtCashAmount);
   console.log(`Transferred ${trasferUsdtCashAmount} USDT tokens to account 2`);
 
-  await usdtCash.transfer(accounts[3], '55000000000000');
+  await usdtCash.transfer(accounts[3], '50000000000000');
   console.log(`Transferred 5000000000 USDT tokens to account 3`);
 
   //coupon.AddressStrategyOne(strategyOneContract.address)
